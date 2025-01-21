6bc8297aaa96fb384dff2f80a574df7bcc03c1d5
Waconewaconeuntitico17 <diazsauradiego@gmail.com>
2025-01-21 02:41:25 -0500

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,169 @@
+def get_by_info(json_file, typ, further_info, categories, logic = "and") :  
+ selected = []
+ all = ["entree", "main_dish", "Side_dish", "desert", "alcoholic", "non_alcoholic"]
+ if categories is None :
+   categories = all  
+ for j in categories :
+    selected.append(get_out_of_category(json_file, typ, further_info, j, logic))
+ return selected
+
+def get_out_of_category(json_file, typ, further_info, category, logic) :
+ selected = [category,] 
+ if further_info is None :
+   
+   for n in json_file.get("menu").get(category).get("items") : 
+     try: 
+      selected.append(json_file.get("menu").get(category).get(typ)[json_file.get("menu").get(category).get("items").index(n)]) 
+     except:
+       None
+ else: 
+  if  logic == "or" : 
+    for n in json_file.get("menu").get(category).get("items") : 
+     for i in further_info : 
+      furth = i.lower()
+      if type(n) is str:
+       if furth in json_file.get("menu").get(category).get("items")[json_file.get("menu").get(category).get("items").index(n)].lower() :
+        selected.append(json_file.get("menu").get(category).get(typ)[json_file.get("menu").get(category).get("items").index(n)])    
+        break
+  
+  if logic == "and" :
+    for n in json_file.get("menu").get(category).get("items") : 
+      and_clause = 0
+      for i in further_info: 
+       furth = i.lower()
+       if type(n) is str: 
+        if furth in json_file.get("menu").get(category).get("items")[json_file.get("menu").get(category).get("items").index(n)].lower() : 
+         and_clause += 1 
+      if and_clause == len(further_info) : 
+        selected.append(json_file.get("menu").get(category).get(typ)[json_file.get("menu").get(category).get("items").index(n)])  
+ 
+ 
+ return selected
+
+def get_median_price(json_file, further_info, categories, logic = "and" ) :
+     import statistics
+     dataset = []
+     for i in get_by_info(json_file, "prices", further_info, categories, logic ) :
+        for j in i :
+          if type(j) is int:
+           dataset.append(j)
+          elif type(j) is float:
+            dataset.append(j) 
+        for i in dataset: 
+         if i is not None : 
+          return statistics.median(dataset)   
+
+def cost_check(json_file, billsplit, costumers, drinks, expcut):
+  all  = ["entree", "main_dish", "desert"]
+  medians = []
+  drink_median = 0 
+  if expcut is True :
+   if billsplit is True:
+    def sucesion(n) : 
+     if n == 1:
+      return 1
+     else : 
+      return (sucesion(n-1) * (n-1) + (n % 2)) / n
+   if billsplit is False: 
+     def sucesion(n) :
+       if n == 1:
+        return 1
+       else : 
+        return (sucesion(n-1) + (n % 2))
+    
+
+   entrees = [] 
+   import statistics
+   all.remove("entree")
+  
+  if get_median_price(json_file, None, ["alcoholic", "non_alcoholic"]) is not None:
+    drink_median = get_median_price(json_file, None, ["alcoholic", "non_alcoholic"])
+  for i in all :
+     if get_median_price(json_file, None, [i] ) is not None : 
+       medians.append(get_median_price(json_file, None, [i] ))
+  if expcut is True :
+   for v in json_file.get("menu").get("entree").get("items") :
+     count = 0
+     if v is not None :  
+      for x in ["crema", "sopa"] : 
+       if x in v :
+        count +=1 
+     if count == 0 : 
+      if (type(json_file.get("menu").get("entree").get("prices")[json_file.get("menu").get("entree").get("items").index(v)]) is int) or (type(json_file.get("menu").get("entree").get("prices")[json_file.get("menu").get("entree").get("items").index(v)]) is float) :
+       entrees.append(json_file.get("menu").get("entree").get("prices")[json_file.get("menu").get("entree").get("items").index(v)])   
+  if medians == [] :
+     medians = 0
+  else :
+     medians = sum(medians)  
+  if expcut is False :
+     if billsplit is True:
+      return (medians + (drink_median * drinks))
+     elif billsplit is False: 
+      return costumers * (medians + (drink_median * drinks))   
+  if (expcut is True) :
+     if entrees != [] :     
+      if billsplit is False:
+       return (costumers * (medians + (drink_median * drinks))) + (statistics.median(entrees) * sucesion(costumers)) 
+      else:
+       return (medians + (drink_median * drinks)) + (statistics.median(entrees) * sucesion(costumers))  
+     else:  
+      if billsplit is False:
+       return costumers * (medians + (drink_median * drinks))
+      if billsplit is True:
+        return (medians + (drink_median * drinks))
+        
+
+def affordabillity_check(json_file, budget, people, drinks, expcut) :
+  return cost_check(json_file, False, people, drinks, expcut) * 100 / budget 
+
+
+def Diversity_within_a_menu(json_file, category, min):
+ tipos = {
+   "Pescado" : ["pescado", "pargo", "bacalao", "canciller"], 
+   "res": ["vaca", "res", "beef", "ropavieja"], 
+   "pollo" : ["pollo", "gordon blue", "gordonblue", "pechuga"], 
+   "cerdo" : ["cerdo", "puerco"],
+   "pizza" : ["pizza"],
+   "pasta" : ["pasta", "lasagna", "lasana", "canelonni", "caneloni", "canelones", "fettuccine", "fettuccini", "spaguetti", "espaguetti", "espagueti", "penne", "macarrones", "maccarroni"],
+   "langosta/camarones" : ["lobster", "langosta", "thermidor", "camar", "gamba", "camarones"],
+   "sandw..." : ["ndwich", "emparedado", "pan con", "pan c/", "hamburguesa", "hotdog", "hot dog"]
+   }
+   
+ length = len(json_file.get("menu").get(category).get("items")) 
+ if length < min :
+   return None
+ #if N == 0:
+  #print(json_file["name"])
+  #print(len(json_file.get("menu").get(category).get("items")) )
+ Suma = 0
+ N = 0
+ compare = []
+ rest = 0
+ for d in tipos :
+   n = 0
+   for c in get_out_of_category(json_file, "items", tipos[d], category, "or") :
+      if c != category:
+        compare.append(c)
+        n += 1
+        N += 1
+   Suma += (n*(n-1))
+ for p in json_file.get("menu").get(category).get("items"):
+  if p not in compare: 
+    rest +=1
+ Suma += (rest*(rest-1))
+ N += rest
+ if N != 0 : 
+  return 1 - (Suma / (N * (N-1)))
+ else :
+   return None
+   
+
+   
+
+
+
+
+
+
+
+
--- ziyech.py
+++ ziyech.py
@@ -1,36 +1,130 @@
 import json
 
+import statistics
 import matplotlib.figure
 import matplotlib.font_manager
 import matplotlib.pyplot
 import functions
 import matplotlib
 from pathlib import Path
+#"Hoteleria y restauracion" : 4564,
 
-wages = {"agricultura" : 4455, "Hoteleria y restauracion" : 4564, "Ciencia innovacion y tecnologia" : 5376, "Educacion": 3932, "Salud" : 4222}
-x = ["plaza de la revolucion", "centro habana", "diez de octubre"]
+matplotlib.rcParams.update({'font.size': 8})
+wages = {"Ciencia innovacion y tecnologia" : 5376, "Hoteleria y restauracion" : 4564, "agricultura" : 4455, "Salud" : 4222,  "Educacion": 3932}
+x = ["Playa", "Plaza de la revolucion", "Centro habana", "Diez de octubre", "Habana Vieja", "Habana del Este"]
 y = []
+mun = ["Playa", "Plaza de la revolucion", "Centro habana", "Diez de octubre", "Habana Vieja", "Habana del Este", "Cerro", "La Lisa", "San Miguel del Padron"]
+tipos = {
+   "fish" : ["pescado", "pargo", "bacalao", "canciller"], 
+   "pork" : ["cerdo", "puerco"],
+   "beef": ["vaca", "res", "beef", "ropavieja"], 
+   "chicken" : ["pollo", "gordon blue", "gordonblue", "pechuga"]
+ }
+
 def cestpartiedutruc(p) :
+  per_wage = []
   for n in x :
-   sampooool = 0
-   tous =0
    for i in Path(n).iterdir() :
-    tous +=1
     with open (i, "r", encoding="utf-8") as truc :
      ouf = json.load(truc)
-     if functions.affordabillity_check(ouf, wages.get(p), 1, 2, "False") is True :
-      sampooool += 1
-   if sampooool > 1:
-    y.append((sampooool * 100) / tous)  
-  if y != []:
-   return y
+     per_wage.append(functions.affordabillity_check(ouf, wages.get(p), 1, 2, True)) 
+   y.append(statistics.mean(per_wage))
+   per_wage.clear()      
+   
+
 
 def cestuntruc() :
  
+ matplotlib.pyplot.figure(figsize = (10,6))
  for v in wages :
-    y.clear()
+    
     cestpartiedutruc(v)
     matplotlib.pyplot.scatter(x,y, label = v) 
-    matplotlib.rcParams.update({'font.size': 8})
-    matplotlib.pyplot.legend()   
+    matplotlib.pyplot.legend() 
+    y.clear()
 
+def pechesansleurre():
+ deselements = ["fish", "pork", "beef", "chicken", "other" ]
+ partie = [0, 0, 0, 0, 0]
+ for i in mun :
+  for l in Path(i).iterdir():
+   with open(l, "r", encoding = "utf-8") as truc:
+      ouf = json.load(truc) 
+      for t in functions.get_out_of_category(ouf, "items", None, "main_dish", "or") :
+           partie[4] += 1
+      for g in tipos :
+       for n in functions.get_out_of_category(ouf, "items", tipos[g], "main_dish", "or") :
+        if (type(n) is not list) and (n != "main_dish") :
+          partie[deselements.index(g)] += 1 
+          partie[4] -= 1
+
+                           
+ matplotlib.pyplot.pie(partie, labels = deselements) 
+
+
+
+def pecheavecleurre():
+ deselements = ["fish", "pork", "beef", "chicken"]
+
+ g_avg = []
+ y_axis = []
+ for g in  tipos:
+  for i in mun :
+   for l in Path(i).iterdir():
+     with open(l, "r", encoding = "utf-8") as truc:
+      ouf = json.load(truc)      
+      for n in functions.get_out_of_category(ouf, "prices", tipos[g], "main_dish", "or") :
+        if (type(n) is not list) and (type(n) is not str) :
+         g_avg.append(n)
+       
+  y_axis.append(statistics.mean(g_avg))
+  g_avg.clear()
+
+ matplotlib.pyplot.bar(deselements, y_axis)
+
+def cestdautre(k) :
+  per_person = []
+  for n in x :
+   for i in Path(n).iterdir() :
+    with open (i, "r", encoding="utf-8") as truc :
+     ouf = json.load(truc)
+     per_person.append(functions.cost_check(ouf,True, 4, 2, k)) 
+   y.append(statistics.mean(per_person))
+   per_person.clear()      
+   
+
+
+def ouaiscestdautre() :
+    matplotlib.pyplot.figure(figsize = (10,6))
+    cestdautre(True)
+    matplotlib.pyplot.scatter(x,y, label = "sharing an entree") 
+    y.clear()
+    cestdautre(False)
+    matplotlib.pyplot.scatter(x,y, label = "whole entree for each") 
+    y.clear()
+    matplotlib.pyplot.legend() 
+
+def troipummons(minim) :
+  checked = []
+  rank = []
+  allrest = {}
+  pos = 0
+  for i in mun :
+   for l in Path(i).iterdir():
+     with open(l, "r", encoding = "utf-8") as truc:
+      ouf = json.load(truc) 
+      if functions.Diversity_within_a_menu(ouf, "main_dish", minim) is not None:
+       rank.append(functions.Diversity_within_a_menu(ouf, "main_dish", minim)) 
+       allrest[ouf.get("name")] = functions.Diversity_within_a_menu(ouf, "main_dish", minim)
+  rank.sort()
+  for n in rank:
+    if pos < 29 : 
+     for t in allrest:
+      if t not in checked:
+       if allrest[t] == n:
+        checked.append(t)
+        pos += 1
+        print(f'{pos}.  {t} {n}')
+   
+
+    
